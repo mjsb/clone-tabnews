373c5d5e14fb5cff359cdc7a45c3921494a45b8c
Marcio Borges <marciojsb@hotmail.com>
2025-04-27 04:24:46 +0000

    Add `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Olá meu amor, tudo bem com você!? O que vamos jantar?</h1> 
+  return <h1>Olá meu amor, tudo bem com você!? O que vamos jantar?</h1>;
 }
 
-export default Home
+function d() {
+  console.log("Identação errada!!!");
+}
+
+export default Home;
