a91960fe5e04c85b111ee7ef86aaeeb743a439f4
Marcio Borges <marciojsb@hotmail.com>
2025-12-30 16:54:48 +0000

    Add comment to test vercel deployment

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,7 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
-
+//comment
 export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
 
